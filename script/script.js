bb60a1d432ed7c3a6cb19ba70a700dc2fd66f18f
LuigiRac <lui.rac06@gmail.com>
2024-11-18 18:03:55 +0100

    end exercise

--- script/script.js
+++ script/script.js
@@ -3,15 +3,44 @@ console.clear();
 
 const extration = document.getElementById("mailGenerate");
 const button = document.querySelector("button");
-
-
+const listUl = document.getElementById("mail-list");
+let arrayMail = [];
 button.addEventListener("click", getMail);
 
 function getMail() {
-    axios
-        .get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then((res) => {
-            console.log(res.data.response);
-            extration.innerHTML = res.data.response
-        });
+    for (let i = 0; i < 10; i++) {
+
+        axios
+            .get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((res) => {
+                // console.log(res.data.response);
+                arrayMail.push(res.data.response);
+                if (arrayMail.length === 10) {
+                    printMail(arrayMail)
+
+                }
+                console.log(arrayMail);
+
+            })
+    }
+
 }
+
+
+function printMail(arMail) {
+    console.log(arMail);
+
+    // let mailList = '';
+    arrayMail.forEach((mail) => {
+        let mailList = document.createElement("li");
+        mailList.innerHTML = mail;
+        // mailList += `<li>${mail}</li>`
+        listUl.appendChild(mailList);
+
+        console.log(listUl);
+    });
+
+}
+// console.log(getMail());
+
+
