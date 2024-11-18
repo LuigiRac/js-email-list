2780ebf7bab24e29a7357f3b922f43c93815fcd3
LuigiRac <lui.rac06@gmail.com>
2024-11-18 13:51:41 +0100

    html + axios(console.log

--- script/script.js
+++ script/script.js
@@ -1,3 +1,12 @@
 "use strict"
 console.clear();
 
+const extration = document.getElementById("mailGenerate");
+const button = document.querySelector("button");
+
+axios
+    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then((res) => {
+        console.log(res.data);
+
+    });
